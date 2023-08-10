643c1b7781d6ae280cc8ddaf78e9f6814638a142
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-08-10 20:23:51 +0900

    main page modify...

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
+
+    const [ userName, setUserName ] = useState('');
+    const [ position, setPosition ] = useState('');
+    const [ reprYn, setReprYn ] = useState('N');
+
+    const navigate = useNavigate();
+
+    // const [] = useState();
+    // const [] = useState([]);
 
     useEffect( () => {
         goBoard();
@@ -13,18 +23,50 @@ const Main = () => {
                 '/api/board/list'
             );
 
-            console.log(response)
+            console.log(response.data[0])
+
+            setUserName(response.data[0].usr_name);
+            setPosition(response.data[0].cod_name);
+            setReprYn(response.data[0].repr_yn);
         } catch (e) {
             console.error(e);
         }
     }
 
+    const goLogout = async () => {
+        try {
+            const response = await axios.get(
+                '/api/user/logout'
+            );
+
+            console.log(response);
+            if (response.status == '200') {
+                navigate('/');
+            }
+        } catch(e) {
+            console.error(e)
+        }
+    }
 
     return (
         <>
-            <h2>Main</h2>
+            <h2>{userName}({position}) 님 환영합니다.</h2>
+            <button type="button" onClick={() => goLogout()}>로그아웃</button> &nbsp;
+            <button type="button">글쓰기</button>  &nbsp;
+            { reprYn == 'Y' && <button>대리결제</button>}
         </>
     )
 }
 
 export default Main;
+
+// brd_content: "업무결재"
+// brd_created_at: "2022-03-01"
+// brd_created_by: "netflix"
+// brd_id: 1
+// brd_status: "B001"
+// cod_id: "A001"
+// cod_name: "사원"
+// repr_yn: "N"
+// usr_name: "이대용"
+// usr_position: "A001"
